3bf3ea5a6954b57ec6797e39152e8d0bda449aea
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-27 14:39:53 +0300

    Add AdderActivity

--- app/src/main/java/com/gpetuhov/android/sampledagger3/CalculatorActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampledagger3/CalculatorActivity.kt
@@ -2,11 +2,15 @@ package com.gpetuhov.android.sampledagger3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pawegio.kandroid.startActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        addButton.setOnClickListener { startActivity<AdderActivity>() }
     }
 }
