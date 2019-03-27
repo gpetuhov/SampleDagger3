264e6a47da25a1e7cb44ea572cace9109bb15070
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-27 14:36:36 +0300

    Add calculator activity

---
+++ app/src/main/java/com/gpetuhov/android/sampledagger3/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.gpetuhov.android.sampledagger3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
--- app/src/main/java/com/gpetuhov/android/sampledagger3/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampledagger3/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gpetuhov.android.sampledagger3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawegio.kandroid.startActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculatorButton.setOnClickListener { startActivity<CalculatorActivity>() }
     }
 }
